37c740ab79597376468bd7c79df2eb026a114037
rozaydin <ridvan.ozaydin@siemens.com>
2018-08-15 20:11:54 +0300

    navigation and main components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { AppComponent } from './app.component';
 
 // layout components
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // material components
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSidenavModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSidenavModule } from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TestFormComponent } from './test-form/test-form.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 // Routes
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '',
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'testto', pathMatch: 'full' },
+      { path: 'testto', component: TestFormComponent },
+    ]
+  },
+  {
+    path: '',
     redirectTo: '/login',
     pathMatch: 'full'
   },
@@ -27,7 +37,9 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    TestFormComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
